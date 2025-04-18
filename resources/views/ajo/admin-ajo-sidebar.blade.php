6db229ebaae6fa5bea9c1f024f6c498c94b7f853
Pelumi <fasanyafemi@gmail.com>
2025-04-18 17:40:46 +0100

    navbar

--- resources/views/ajo/admin-ajo-sidebar.blade.php
+++ resources/views/ajo/admin-ajo-sidebar.blade.php
@@ -1,11 +1,10 @@
 <ul class="navbar-nav" id="navbar-nav">
-
     <li class="menu-title">General</li>
 
     <li class="nav-item">
          <a class="nav-link" href="/dashboard">
               <span class="nav-icon">
-                   <iconify-icon icon="solar:widget-5-bold-duotone"></iconify-icon>
+                   <iconify-icon icon="solar:chart-2-bold-duotone"></iconify-icon>
               </span>
               <span class="nav-text"> Dashboard </span>
          </a>
@@ -14,7 +13,7 @@
     <li class="nav-item">
          <a class="nav-link " href="/my-profile">
               <span class="nav-icon">
-                   <iconify-icon icon="solar:user-bold-duotone"></iconify-icon>
+                   <iconify-icon icon="solar:user-id-bold-duotone"></iconify-icon>
               </span>
               <span class="nav-text"> Profile </span>
          </a>
@@ -22,7 +21,7 @@
     <li class="nav-item">
          <a class="nav-link " href="/admin/group">
               <span class="nav-icon">
-                   <iconify-icon icon="solar:user-bold-duotone"></iconify-icon>
+                   <iconify-icon icon="solar:users-group-two-rounded-bold-duotone"></iconify-icon>
               </span>
               <span class="nav-text"> Contribution Group(s) </span>
          </a>
@@ -30,7 +29,7 @@
     <li class="nav-item">
          <a class="nav-link " href="/admin/group/contribution-dues">
               <span class="nav-icon">
-                   <iconify-icon icon="solar:user-bold-duotone"></iconify-icon>
+                   <iconify-icon icon="solar:wallet-money-bold-duotone"></iconify-icon>
               </span>
               <span class="nav-text"> My Dues </span>
          </a>
@@ -41,7 +40,7 @@
     <li class="nav-item">
          <a class="nav-link" href="https://wa.me/2349058744473">
               <span class="nav-icon">
-                   <iconify-icon icon="solar:help-bold-duotone"></iconify-icon>
+                   <iconify-icon icon="solar:chat-round-dots-bold-duotone"></iconify-icon>
               </span>
               <span class="nav-text"> Help Center </span>
          </a>
@@ -50,18 +49,16 @@
     <li class="nav-item">
          <a class="nav-link" href="#">
               <span class="nav-icon">
-                   <iconify-icon icon="solar:question-circle-bold-duotone"></iconify-icon>
+                   <iconify-icon icon="solar:clipboard-list-bold-duotone"></iconify-icon>
               </span>
               <span class="nav-text"> FAQs </span>
          </a>
     </li>
 
-
-
     <li class="nav-item">
          <a class="nav-link" href="/logout" onclick="return confirm('Are you sure you want to sign out?')">
               <span class="nav-icon">
-                   <iconify-icon icon="solar:user-block-rounded-bold-duotone"></iconify-icon>
+                   <iconify-icon icon="solar:logout-2-bold-duotone"></iconify-icon>
               </span>
               <span class="nav-text"> Logout </span>
          </a>
--- resources/views/ajo/admin-coop-sidebar.blade.php
+++ resources/views/ajo/admin-coop-sidebar.blade.php
@@ -1,55 +1,46 @@
 <ul class="navbar-nav" id="navbar-nav">
-
     <li class="menu-title">General</li>
 
     <li class="nav-item">
          <a class="nav-link" href="/dashboard">
               <span class="nav-icon">
-                   <iconify-icon icon="solar:widget-5-bold-duotone"></iconify-icon>
+                   <iconify-icon icon="solar:chart-2-bold-duotone"></iconify-icon>
               </span>
               <span class="nav-text"> Dashboard </span>
          </a>
     </li>
 
     <li class="nav-item">
-         <a class="nav-link " href="/my-profile">
+         <a class="nav-link" href="/my-profile">
               <span class="nav-icon">
-                   <iconify-icon icon="solar:user-bold-duotone"></iconify-icon>
+                   <iconify-icon icon="solar:user-id-bold-duotone"></iconify-icon>
               </span>
               <span class="nav-text"> Profile </span>
          </a>
     </li>
 
-  
-
-
-
     <li class="nav-item">
          <a class="nav-link" href="/admin/member" role="button" aria-expanded="false">
               <span class="nav-icon">
-                   <iconify-icon icon="solar:bag-smile-bold-duotone"></iconify-icon>
+                   <iconify-icon icon="solar:users-group-rounded-bold-duotone"></iconify-icon>
               </span>
               <span class="nav-text"> Members </span>
          </a>
-       
     </li>
+
     <li class="nav-item">
-         <a class="nav-link" href="/admin/plan"  role="button">
+         <a class="nav-link" href="/admin/plan" role="button">
               <span class="nav-icon">
-               
-              <iconify-icon icon="solar:settings-bold-duotone"></iconify-icon>
+                   <iconify-icon icon="solar:tuning-square-bold-duotone"></iconify-icon>
               </span>
               <span class="nav-text"> Settings </span>
          </a>
-        
     </li>
-
-
 
     <li class="nav-item">
          <a class="nav-link menu-arrow" href="#sidebarMapss" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarMaps">
               <span class="nav-icon">
-                   <iconify-icon icon="solar:streets-map-point-bold-duotone"></iconify-icon>
+                   <iconify-icon icon="solar:hand-money-bold-duotone"></iconify-icon>
               </span>
               <span class="nav-text"> Manage Loan</span>
          </a>
@@ -67,30 +58,19 @@
                    <li class="sub-nav-item">
                         <a class="sub-nav-link" href="/admin/application/completed">Completed Loan Application</a>
                    </li>
-                   {{--  <li class="sub-nav-item">
-                        <a class="sub-nav-link" href="/admin/transaction/all">All</a>
-                   </li>
-                   <li class="sub-nav-item">
-                        <a class="sub-nav-link" href="/admin/transaction/form">Form</a>
-                   </li>
-                   <li class="sub-nav-item">
-                        <a class="sub-nav-link" href="/admin/transaction/repayment">Repayment</a>
-                   </li>  --}}
               </ul>
          </div>
     </li>
+
     <li class="nav-item">
          <a class="nav-link menu-arrow" href="#sidebarMaps" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarMaps">
               <span class="nav-icon">
-                   <iconify-icon icon="solar:streets-map-point-bold-duotone"></iconify-icon>
+                   <iconify-icon icon="solar:card-bold-duotone"></iconify-icon>
               </span>
               <span class="nav-text"> Manage Transactions </span>
          </a>
          <div class="collapse" id="sidebarMaps">
               <ul class="nav sub-navbar-nav">
-                   {{--  <li class="sub-nav-item">
-                        <a class="sub-nav-link" href="/admin/application">Loan Applications</a>
-                   </li>  --}}
                    <li class="sub-nav-item">
                         <a class="sub-nav-link" href="/admin/transaction/registration">Registration</a>
                    </li>
@@ -110,14 +90,12 @@
          </div>
     </li>
 
-
-
     <li class="menu-title mt-2">Support</li>
 
     <li class="nav-item">
          <a class="nav-link" href="https://wa.me/2349058744473">
               <span class="nav-icon">
-                   <iconify-icon icon="solar:help-bold-duotone"></iconify-icon>
+                   <iconify-icon icon="solar:chat-round-dots-bold-duotone"></iconify-icon>
               </span>
               <span class="nav-text"> Help Center </span>
          </a>
@@ -126,18 +104,16 @@
     <li class="nav-item">
          <a class="nav-link" href="#">
               <span class="nav-icon">
-                   <iconify-icon icon="solar:question-circle-bold-duotone"></iconify-icon>
+                   <iconify-icon icon="solar:clipboard-list-bold-duotone"></iconify-icon>
               </span>
               <span class="nav-text"> FAQs </span>
          </a>
     </li>
 
-
-
     <li class="nav-item">
          <a class="nav-link" href="/logout" onclick="return confirm('Are you sure you want to sign out?')">
               <span class="nav-icon">
-                   <iconify-icon icon="solar:user-block-rounded-bold-duotone"></iconify-icon>
+                   <iconify-icon icon="solar:logout-2-bold-duotone"></iconify-icon>
               </span>
               <span class="nav-text"> Logout </span>
          </a>
