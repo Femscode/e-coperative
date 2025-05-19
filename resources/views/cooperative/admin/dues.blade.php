83697493dca1b5ed71395d1dd9fbe7d083ba3f3c
Pelumi <fasanyafemi@gmail.com>
2025-05-19 14:16:58 +0100

    loan display

--- resources/views/cooperative/admin/dues.blade.php
+++ resources/views/cooperative/admin/dues.blade.php
@@ -7,27 +7,34 @@
 <div class="container-fluid">
     <!-- start page title -->
     <div class="col-12 mb-4">
-            <div class="nav-tabs-custom">
-                <div class="d-flex w-100 overflow-hidden">
-                    <a href="/admin/transaction/all" class="nav-link flex-fill text-center py-3">
+        <div class="nav-tabs-custom">
+            <div class="row g-2">
+                <div class="col-6">
+                    <a href="/admin/transaction/all" class="nav-link text-center py-3 h-100">
                         <div class="d-flex align-items-center justify-content-center">
                             <i class="ri-exchange-dollar-line fs-22 me-2"></i>
                             <h5 class="mb-0">All</h5>
                         </div>
                     </a>
-                    <a href="/admin/transaction/registration" class="nav-link flex-fill text-center py-3">
+                </div>
+                <div class="col-6">
+                    <a href="/admin/transaction/registration" class="nav-link text-center py-3 h-100 ">
                         <div class="d-flex align-items-center justify-content-center">
                             <i class="ri-user-add-line fs-22 me-2"></i>
                             <h5 class="mb-0">Registration</h5>
                         </div>
                     </a>
-                    <a href="/admin/transaction/monthly_dues" class="nav-link flex-fill text-center py-3 active">
+                </div>
+                <div class="col-6">
+                    <a href="/admin/transaction/monthly_dues" class="nav-link text-center active py-3 h-100 ">
                         <div class="d-flex align-items-center justify-content-center">
                             <i class="ri-calendar-check-line fs-22 me-2"></i>
                             <h5 class="mb-0">Dues</h5>
                         </div>
                     </a>
-                    <a href="/admin/transaction/repayment" class="nav-link flex-fill text-center py-3">
+                </div>
+                <div class="col-6">
+                    <a href="/admin/transaction/repayment" class="nav-link text-center py-3 h-100">
                         <div class="d-flex align-items-center justify-content-center">
                             <i class="ri-refund-2-line fs-22 me-2"></i>
                             <h5 class="mb-0">Repayments</h5>
@@ -36,6 +43,7 @@
                 </div>
             </div>
         </div>
+    </div>
     <!-- end page title -->
     @livewire('monthly-transactions')
 </div>
